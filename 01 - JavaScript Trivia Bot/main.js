614de644534f88a627f53c635cadeda0227ac4cd
ebagabee <ebagabe.2025@gmail.com>
2025-03-30 15:16:05 -0300

    first project: trivia bot finished

---
+++ 01 - JavaScript Trivia Bot/main.js
@@ -0,0 +1,29 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Trivia Bot",
+  botLocation = "Deep of Internet",
+  favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I like " + favoriteLanguage + " because it's yellow :)";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is very funny";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is all about Internet and now in Backend!";
+
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    "."
+);
