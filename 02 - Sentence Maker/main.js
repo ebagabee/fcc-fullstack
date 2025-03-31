24102e395b84c7ab8eed8283d1d27f12b2cd2833
ebagabee <ebagabe.2025@gmail.com>
2025-03-31 08:18:59 -0300

    feat: second task is done

---
+++ 02 - Sentence Maker/main.js
@@ -0,0 +1,22 @@
+let 
+adjective = "Good",
+noun = 'Chair', 
+verb = "do", 
+place = "Home", 
+adjective2 = "Bad", 
+noun2 = "orange";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "best";
+noun = "tiger";
+verb = "sleep";
+place = "hospital";
+adjective2 = "strong";
+noun2 = "lyon";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: "+ secondStory);
